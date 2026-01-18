de4c1ecdae8ba65496ccb26a5f3dd5d20ea432e1
sankalp <sankalpdixitiiit@gmail.com>
2026-01-18 15:55:13 +0530

    fix6

--- src/controller/swipe.controller.ts
+++ src/controller/swipe.controller.ts
@@ -2,7 +2,7 @@ import { Response } from 'express';
 import prisma from '../config/database.config';
 import { AuthRequest } from '../types';
 import { extractPublicIdFromUrl, deleteResourcesFromCloudinary } from '../services/cloudinary.service';
-import { notifyNewMatch, notifyNewLike, notifyUnmatch } from '../services/notification.service';
+import { notifyNewMatch, notifyNewLike } from '../services/notification.service';
 
 /**
  * Record a swipe action and check for mutual match
@@ -433,7 +433,7 @@ export const getSwipeProfiles = async (req: AuthRequest, res: Response): Promise
         });
         const userLat = currentUser?.profile?.latitude;
         const userLon = currentUser?.profile?.longitude;
-        
+
         console.log(`[getSwipeProfiles] Current user location:`, {
             userId,
             hasProfile: !!currentUser?.profile,
